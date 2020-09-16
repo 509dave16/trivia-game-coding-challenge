8652cecf216d806f34f00953e354567d827da0a2
David Fall <david@madebycodera.com>
2020-09-16 00:44:32 -0700

    TGCC - Implemented "Results" screen with similar approach to "Intro" screen, reusing where possible

--- src/components/screens/Results.tsx
+++ src/components/screens/Results.tsx
@@ -1,14 +1,72 @@
 import * as React from 'react'
-import { View, Text } from 'react-native'
+import { View, FlatList } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Button } from 'material-bread'
+import { ListItem, Icon, withTheme } from 'material-bread'
 
-function Results({ navigation }) {
+import Heading from '~components/widgets/Heading'
+import Button from '~components/widgets/Button'
+
+import sharedStyles from '~config/sharedStyles'
+import colors from '~config/colors'
+
+const defaultQuestions = [
+  {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "hard",
+    "question": "Unturned originally started as a Roblox game.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+      "False"
+    ],
+    "correct": false,
+  }
+]
+
+interface QuestionResultProps {
+  theme: any;
+  question: any;
+  text: string;
+}
+
+const QuestionResult = withTheme(({ theme, question, text }: QuestionResultProps) => {
+  const iconName = question.correct ? 'check-circle' : 'cancel'
+  return (
+    <ListItem
+      icon={<Icon name={iconName} size={30} color={colors.primary}/>}
+      text={text}
+    />
+  )
+})
+
+function renderItem({ item, index }) {
+  return (<QuestionResult key={item.question} question={item} text={item.question}/>)
+}
+
+const itemHeight = 68
+function getItemLayout(data, index) {
+  return { length: itemHeight, offset: itemHeight * index, index }
+}
+
+function keyExtractor(question) {
+  return `${question.question}`
+}
+
+function Results({ navigation, questions = defaultQuestions }) {
   const goToIntro = React.useCallback(() => navigation.navigate('Intro'), [])
   return (
-    <SafeAreaView>
-      <Text>Results screen!</Text>
-      <Button onPress={goToIntro} text="Go to Intro" />
+    <SafeAreaView style={[sharedStyles.screenPadding, sharedStyles.spaceBetweenVertically]}>
+      <View>
+        <Heading style={sharedStyles.textTitle} text="You scored" />
+        <Heading style={sharedStyles.textTitle} text="3 / 10" />
+      </View>
+      <FlatList
+        data={questions}
+        renderItem={renderItem}
+        getItemLayout={getItemLayout}
+        keyExtractor={keyExtractor}
+      />
+      <Button onPress={goToIntro} text="PLAY AGAIN?" />
     </SafeAreaView>
   )
 }
